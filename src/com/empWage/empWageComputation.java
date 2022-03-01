63b480ed3a8b9067055fbf6f99a2744c98088309
Amet-Vikram <amet1197@gmail.com>
2022-03-01 21:03:15 +0530

    added variables

--- src/com/empWage/empWageComputation.java
+++ src/com/empWage/empWageComputation.java
@@ -1,11 +1,10 @@
-package com.EmployeeAttendanceUC;
+package com.empWage;
 
-public class Main {
-
+public class empWageComputation {
     public static void main(String[] args) {
+        int Is_present = 1;
         double empCheck = Math.floor((Math.random()*100)%2);
-
-        if (empCheck == 1)
+        if (empCheck == Is_present)
             System.out.println("Employee is Present ");
         else
             System.out.println("Employee is absent");
