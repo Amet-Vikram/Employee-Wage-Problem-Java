478151a250709cf1fb0981a6590cb8e79298d9e3
Amet-Vikram <amet1197@gmail.com>
2022-03-16 01:47:33 +0530

    Implemented interface for EmpWageBuilder

--- src/com/empWage/EmpWageBuilder.java
+++ src/com/empWage/EmpWageBuilder.java
@@ -2,15 +2,23 @@ package com.empWage;
 
 import java.util.ArrayList;
 
-public class EmpWageBuilder  {
+interface InterfaceCalculateWage {
+    void addRecord(String companyName, int empWagePerHour, int workingHours, int workingDays);
+    void getRecord();
+}
 
-    private ArrayList<CompanyEmpWage> record = new ArrayList();
+public class EmpWageBuilder implements InterfaceCalculateWage {
 
-    void addRecord(CompanyEmpWage o){
+    final private ArrayList<CompanyEmpWage> record = new ArrayList();
+
+    @Override
+    public void addRecord(String name, int workDays, int workHours, int wage){
+        CompanyEmpWage o = new CompanyEmpWage(name, workDays, workHours, wage);
         record.add(o);
     }
 
-    void getRecord(){
+    @Override
+    public void getRecord(){
         for (CompanyEmpWage company : record) {
             System.out.println("Company: " + company.nameOfCompany);
             System.out.println("Days Worked: " + company.totalDays);
